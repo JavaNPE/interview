8f139d9ffc051ec6721c2f7ff911e0152476c5a5
dali <daki9981@qq.com>
2021-05-16 21:36:48 +0800

    单例模式在多线程环境下可能存在安全问题

---
+++ Volatile/src/main/java/com/youliao/java/HashTableTest.java
@@ -0,0 +1,22 @@
+package com.youliao.java;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @Author Dali
+ * @Date 2021/5/16 16:45
+ * @Version 1.0
+ * @Description
+ */
+public class HashTableTest {
+    public static void main(String[] args) {
+        Hashtable<Object, Object> objectObjectHashtable = new Hashtable<>();
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap<>();
+
+    }
+}
---
+++ Volatile/src/main/java/com/youliao/java/SingletonDemo.java
@@ -0,0 +1,43 @@
+package com.youliao.java;
+
+import com.sun.org.apache.bcel.internal.generic.SIPUSH;
+import sun.reflect.generics.tree.ShortSignature;
+
+import java.lang.management.ThreadInfo;
+
+/**
+ * @Author Dali
+ * @Date 2021/5/14 21:14
+ * @Version 1.0
+ * @Description :单例模式在多线程环境下可能存在安全问题
+ */
+public class SingletonDemo {
+    private static SingletonDemo instance = null;
+    private SingletonDemo(){
+        System.out.println(Thread.currentThread().getName()+"\t 我是构造方法SingletonDemo()");
+    }
+    public static SingletonDemo getInstance() {
+        if (instance == null){
+            instance = new SingletonDemo();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+
+        //单线程（main线程的操作动作）
+//        System.out.println(SingletonDemo.getInstance() ==  SingletonDemo.getInstance());
+//        System.out.println(SingletonDemo.getInstance() ==  SingletonDemo.getInstance());
+//        System.out.println(SingletonDemo.getInstance() ==  SingletonDemo.getInstance());
+//        System.out.println();
+//        System.out.println();
+//        System.out.println();
+
+        //并发多线程后，情况发生了很大的变化
+        for (int i = 0; i <= 10; i++) {
+            new Thread(()->{
+                SingletonDemo. getInstance();
+            },String.valueOf(i)).start();
+        }
+    }
+}
