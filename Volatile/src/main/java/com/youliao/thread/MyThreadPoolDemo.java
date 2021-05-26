63d8b16487487cb3e493e0d6d8cc4b2cd0a24896
dali <daki9981@qq.com>
2021-05-26 18:19:38 +0800

    手写线程池

--- Volatile/src/main/java/com/youliao/thread/MyThreadPoolDemo.java
+++ Volatile/src/main/java/com/youliao/thread/MyThreadPoolDemo.java
@@ -1,8 +1,6 @@
 package com.youliao.thread;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * @Author Dali
@@ -12,21 +10,49 @@ import java.util.concurrent.Executors;
  */
 public class MyThreadPoolDemo {
     public static void main(String[] args) {
+        //threadPoolInit(); //jdk版本
+        ExecutorService threadPool = new ThreadPoolExecutor(
+                2,
+                5,
+                1L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(3),
+                Executors.defaultThreadFactory(),
+                //new ThreadPoolExecutor.AbortPolicy());  //默认：终止策略
+//                new ThreadPoolExecutor.CallerRunsPolicy());   // 调用者执行策略
+//                new ThreadPoolExecutor.DiscardOldestPolicy());   // 抛弃队列中等待最久的任务
+                new ThreadPoolExecutor.DiscardPolicy());   // 抛弃策略
+        try {
+            //模拟10个用户来办理业务，每个用户就是-一个来自外部的请求线程
+            for (int i = 1; i <= 9; i++) {
+                threadPool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "\t 办理业务");
+                });
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+
+    private static void threadPoolInit() {
         //System.out.println(Runtime.getRuntime().availableProcessors());
 //        ExecutorService threadPool = Executors.newFixedThreadPool(5);   //一池5个处理线程
 //        ExecutorService threadPool = Executors.newSingleThreadExecutor();   //一池1个处理线程
         ExecutorService threadPool = Executors.newCachedThreadPool();   //一池N个处理线程
         try {
             //模拟10个用户来办理业务，每个用户就是-一个来自外部的请求线程
-            for (int i = 1; i <=10 ; i++) {
-                threadPool.execute(()->{
-                    System.out.println(Thread.currentThread().getName()+"\t 办理业务");
+            for (int i = 1; i <= 10; i++) {
+                threadPool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "\t 办理业务");
                 });
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             threadPool.shutdown();
         }
     }
