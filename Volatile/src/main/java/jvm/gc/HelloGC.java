3df522be2673f47817a0c75c38c257f3fd16ea9b
dali <daki9981@qq.com>
2021-05-27 19:42:57 +0800

    JVM的XX参数之布尔类型

---
+++ Volatile/src/main/java/jvm/gc/HelloGC.java
@@ -0,0 +1,14 @@
+package jvm.gc;
+
+/**
+ * @Author Dali
+ * @Date 2021/5/27 12:48
+ * @Version 1.0
+ * @Description: 60_ JVM的XX参数之布尔类型 https://www.bilibili.com/video/BV18b411M7xz?p=60&spm_id_from=pageDriver
+ */
+public class HelloGC {
+    public static void main(String[] args) throws Exception {
+        System.out.println("**********HelloGC");
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+}
