3433f604cc0f9bcc76e8455cc9a88ee699818680
dali <daki9981@qq.com>
2021-05-28 08:28:00 +0800

    开发中常用的JVM调优参数

--- Volatile/src/main/java/jvm/gc/HelloGC.java
+++ Volatile/src/main/java/jvm/gc/HelloGC.java
@@ -10,16 +10,16 @@ package jvm.gc;
  */
 public class HelloGC {
     public static void main(String[] args) throws Exception {
-        /*System.out.println("**********HelloGC");
-        Thread.sleep(Integer.MAX_VALUE);*/
+        System.out.println("**********HelloGC");
+        //Thread.sleep(Integer.MAX_VALUE);
 
-        // 返回Java虚拟机中内存的总量
+     /*   // 返回Java虚拟机中内存的总量
         long totalMemory = Runtime.getRuntime().totalMemory();
 
         // 返回Java虚拟机中试图使用的最大内存量
         long maxMemory = Runtime.getRuntime().maxMemory();
 
         System.out.println("TOTAL_MEMORY(-Xms) = " + totalMemory + "(字节)、" + (totalMemory / (double) 1024 / 1024) + "MB");
-        System.out.println("MAX_MEMORY(-Xmx) = " + maxMemory + "(字节)、" + (maxMemory / (double) 1024 / 1024) + "MB");
+        System.out.println("MAX_MEMORY(-Xmx) = " + maxMemory + "(字节)、" + (maxMemory / (double) 1024 / 1024) + "MB");*/
     }
 }
