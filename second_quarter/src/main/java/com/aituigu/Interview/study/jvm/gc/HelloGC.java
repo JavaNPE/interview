b394a27ae646bc54092d323fd5c89e0ce92eb1a3
HedianTea <daki9981@qq.com>
2024-08-27 13:31:41 +0800

    分析JVM heap 内存模型

--- second_quarter/src/main/java/com/aituigu/Interview/study/jvm/gc/HelloGC.java
+++ second_quarter/src/main/java/com/aituigu/Interview/study/jvm/gc/HelloGC.java
@@ -21,5 +21,18 @@ public class HelloGC {
 
         System.out.println("TOTAL_MEMORY(-Xms) = " + totalMemory + "(字节)、" + (totalMemory / (double) 1024 / 1024) + "MB");
         System.out.println("MAX_MEMORY(-Xmx) = " + maxMemory + "(字节)、" + (maxMemory / (double) 1024 / 1024) + "MB");*/
+
+        /**
+         * -Xms 设置初始分配大小，默认为物理内存的1/64
+         * -Xmx 最大分配内存，默认为物理内存的1/4
+         * -XX:+PrintGCDetails 输出详细的GC处理日志
+         * 注意：参数调优一定要初始大小和最大分配内存的大小要一致
+         */
+        // VM参数： -Xms1024m -Xmx1024m -XX:+PrintGCDetails
+        long maxMemory = Runtime.getRuntime().maxMemory();//返回 Java 虚拟机试图使用的最大内存量。
+        long totalMemory = Runtime.getRuntime().totalMemory();//返回 Java 虚拟机中的内存总量。
+        System.out.println("MAX_MEMORY = " + maxMemory + "（字节）、" + (maxMemory / (double) 1024 / 1024) + "MB");
+        System.out.println("TOTAL_MEMORY = " + totalMemory + "（字节）、" + (totalMemory / (double) 1024 / 1024) + "MB");
+
     }
 }
